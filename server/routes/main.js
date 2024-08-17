ffbce6d25e9f001d8ff7cec76be9158ecd7f843d
Porky <123284870+MadManJJ@users.noreply.github.com>
2024-08-18 01:11:31 +0700

    footer and header a bit

---
+++ server/routes/main.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+//Routes
+router.get('', (req, res) => {
+    const locals = {
+        title: "NodeJS Blog",
+        description: "Simple Blog created with Nodejs, Express & MongoDb."
+    }
+    const x = "bob";
+
+
+    res.render('index',{ locals,x }); //index.ejs
+});
+
+router.get('/about', (req, res) => {
+    res.render('about'); //index.ejs
+});
+
+module.exports = router;
